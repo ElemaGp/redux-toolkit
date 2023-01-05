d58f9ebc3eb6e2f1ca6989fb2ece73a4d8078081
ElemaGp <elemagodspower@gmail.com>
2023-01-05 14:22:46 +0100

    feat: created the store for the redux reducers

---
+++ src/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+// import cakeReducer from '../features/cake/cakeSlice'
+// import icecreamReducer from '../features/icecream/icecreamSlice'
+import userReducer from '../features/user/userSlice'
+
+const store = configureStore({
+  reducer: {
+    // cake: cakeReducer,
+    // icecream: icecreamReducer,
+    user: userReducer  //the name of the reducer in the userSlice.js is "user" so i used that here as the key (key/value pair), and then i imported the reducer function from the userSlice.js as "UserReducer" and used that here too as the value. (key/value pair).
+  }
+})
+
+export default store
