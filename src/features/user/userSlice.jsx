97a3fda0edaa96c65171427dcc27e011e6f5ada1
ElemaGp <elemagodspower@gmail.com>
2023-01-05 14:13:25 +0100

    README updated and more comments added to the userSlice page

--- src/features/user/userSlice.jsx
+++ src/features/user/userSlice.jsx
@@ -1,12 +1,15 @@
 import axios from 'axios'
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
+//initial state section below
 const initialState = {
   loading: false,
   users: [],
   error: ''
 }
 
+
+//actions and doing the async/api call section below
 // createAsyncThunk automatically generates pending, fulfilled and rejected action types aka "start, success, failure"
 export const fetchUsers = createAsyncThunk('user/fetchUsers', () => { //"user/fetchUsers" is the "action"
   return axios
@@ -14,11 +17,17 @@ export const fetchUsers = createAsyncThunk('user/fetchUsers', () => { //"user/fe
     .then(response => response.data)
 })
 
+//you dont need a "try, catch" block for redux. Redux will automatically catch the error.
+//for login, "const fetchUsers" will be equivalent to "const login".
+
+
+
+//reducer section below
 const userSlice = createSlice({
   name: 'user',
   initialState,
   extraReducers: builder => {
-    builder.addCase(fetchUsers.pending, state => {
+    builder.addCase(fetchUsers.pending, state => {  //this means that if the case of the action is fetchUser.pending (ie fetchUsers_start), "state.loading" becomes true
       state.loading = true
     })
     builder.addCase(fetchUsers.fulfilled, (state, action) => {
