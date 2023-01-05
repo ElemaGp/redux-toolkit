36c0aff1338d7a387b044b355ddf87b7fb46a6dd
ElemaGp <elemagodspower@gmail.com>
2023-01-05 15:55:51 +0100

    feat: dispatched the fetchUsers action creator in Home page to get list of users

--- src/features/user/userSlice.jsx
+++ src/features/user/userSlice.jsx
@@ -1,3 +1,5 @@
+//This userSlice shows how to use rtk for async operations eg that have to do with APIs.
+
 import axios from 'axios'
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
@@ -11,7 +13,7 @@ const initialState = {
 
 //actions and doing the async/api call section below
 // createAsyncThunk automatically generates pending, fulfilled and rejected action types aka "start, success, failure"
-export const fetchUsers = createAsyncThunk('user/fetchUsers', () => { //"user/fetchUsers" is the "action"
+export const fetchUsers = createAsyncThunk('user/fetchUsers', () => { // "user/fetchUsers" is the "action". You derive it by mixing the name of the slice (check the slice below which is "user") and the name of the action-creator function which is "fetchUsers" here. Thereby giving you "user/fetchUsers"
   return axios
     .get('https://jsonplaceholder.typicode.com/users')
     .then(response => response.data)
@@ -19,6 +21,7 @@ export const fetchUsers = createAsyncThunk('user/fetchUsers', () => { //"user/fe
 
 //you dont need a "try, catch" block for redux. Redux will automatically catch the error.
 //for login, "const fetchUsers" will be equivalent to "const login".
+//So basically, when the fetchUser action creator function is dispatched from the desired component, it dispatched this "user/fetchUsers" action. Then while that action is being dispached, when it is pending/start, the reducer values of the loading, user and error states take their values. When it is fulfulfiled/success, the reducer values of the loading, user and error states take their values. If there's a failure/rejected, the the reducer values of the loading, user and error states take their values.
 
 
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,32 @@
 import "./home.css"
 import React from 'react'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchUsers } from "../../features/user/userSlice"
+
 
 const Home = () => {
+
+  const user = useSelector(state => state.user)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchUsers()) //dispatching the "fetchUser" action-creator function in userSlice, which will then dispach the action "user/fetchUsers" in userSlice.jsx.
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>
+      <h2>List of Users</h2>
+      {user.loading && <div>Loading...</div>}
+      {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+      {!user.loading && user.users.length ? (
+        <ul>
+          {user.users.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
   )
 }
 
